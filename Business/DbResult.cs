8008e230e3427abac45ea37a6b02124da9f7df5d
Scott Parkhill <scottparkhill@culturefoundrystudios.com>
2023-02-24 18:06:07 -0500

    Add DbResult class

---
+++ Business/DbResult.cs
@@ -0,0 +1,11 @@
+namespace Chaos.Business;
+
+public class DbResult
+{
+    public TaskResults Result { get; set; }
+    public string Message { get; set; }
+
+    public DbResult(TaskResults result, string message) => (Result, Message) = (result, message);
+
+    public void Deconstruct(out TaskResults result, out string message) => (result, message) = (Result, Message);
+}
