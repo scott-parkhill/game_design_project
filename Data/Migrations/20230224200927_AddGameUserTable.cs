1f0685a1bc0807dc3b5f3a2280d9c81ab850df9b
Scott Parkhill <scottparkhill@culturefoundrystudios.com>
2023-02-24 15:23:43 -0500

    Apply migration to add GameUser to db

---
+++ Data/Migrations/20230224200927_AddGameUserTable.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Chaos.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddGameUserTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "Faction",
+                table: "AspNetUsers",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Faction",
+                table: "AspNetUsers");
+        }
+    }
+}
