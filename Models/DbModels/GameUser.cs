766aa14b6774c7dfacbcb3313026406b119356c0
Scott Parkhill <scottparkhill@culturefoundrystudios.com>
2023-02-24 15:23:13 -0500

    Add GameUser db model

--- Models/DbModels/GameUser.cs
+++ Models/DbModels/GameUser.cs
@@ -1,8 +1,9 @@
 using Microsoft.AspNetCore.Identity;
+using Chaos.Business;
 
 namespace Chaos.Models.DbModels;
 
 public class GameUser : IdentityUser
 {
-
+    public Factions Faction { get; set; }
 }
