db69b9fbe072c79a60b0b01f442ba23d95591533
Scott Parkhill <scottparkhill@culturefoundrystudios.com>
2023-02-24 15:23:02 -0500

    Add GameDbContext.cs

--- Data/GameDbContext.cs
+++ Data/GameDbContext.cs
@@ -1,12 +1,15 @@
 ﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using Chaos.Models.DbModels;
 
 namespace Chaos.Data;
 
-public class GameDbContext : IdentityDbContext
+public class GameDbContext : IdentityDbContext<GameUser>
 {
     public GameDbContext(DbContextOptions<GameDbContext> options)
         : base(options)
     {
     }
+
+    public DbSet<GameUser> GameUsers => Set<GameUser>();
 }
