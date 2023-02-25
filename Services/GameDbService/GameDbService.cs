731d2d6ad7052a67ee84d8eb4908a5c09c1abd7b
Scott Parkhill <scottparkhill@culturefoundrystudios.com>
2023-02-25 17:08:06 -0500

    Fix typo in GameDbService constructor

--- Services/GameDbService/GameDbService.cs
+++ Services/GameDbService/GameDbService.cs
@@ -5,6 +5,6 @@ namespace Chaos.Services.GameDbService;
 public partial class GameDbService : IGameDbService
 {
     readonly GameDbContext _context;
-    readonly Logger<GameDbService> _logger;
-    public GameDbService(GameDbContext context, Logger<GameDbService> logger) => (_context, _logger) = (context, logger);
+    readonly ILogger<GameDbService> _logger;
+    public GameDbService(GameDbContext context, ILogger<GameDbService> logger) => (_context, _logger) = (context, logger);
 }
