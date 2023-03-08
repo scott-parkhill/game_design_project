499ec1f972b69ba68d0ef73f3340f5b725f022ff
Scott Parkhill <scottparkhill@culturefoundrystudios.com>
2023-03-07 23:37:43 -0500

    Add new models to db context

--- Data/GameDbContext.cs
+++ Data/GameDbContext.cs
@@ -12,4 +12,14 @@ public class GameDbContext : IdentityDbContext<GameUser>
     }
 
     public DbSet<GameUser> GameUsers => Set<GameUser>();
+    public DbSet<Army> Armies => Set<Army>();
+    public DbSet<UserArmy> UserArmies => Set<UserArmy>();
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        base.OnModelCreating(builder);
+
+        // Set foreign keys and other database parameters.
+        builder.Entity<UserArmy>().HasKey(u => new { u.UserId, u.ArmyId });
+    }
 }
