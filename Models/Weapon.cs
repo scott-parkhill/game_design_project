f545a61cbe04c5c2c7842601959391b31af69fb8
nmorri <nmorri47@uwo.ca>
2023-03-25 12:06:52 -0400

    Sapper and Sentry Tools Added

--- Models/Weapon.cs
+++ Models/Weapon.cs
@@ -35,6 +35,24 @@ public record Weapon(string Name, string DolphinName, ActionTypes ActionType, in
         { WeaponTypes.BlackbeardsTunic, new("Blackbeard's Tunic", "Sea Urchin Mail", ActionTypes.Defense, 500_000, 60_000)},
         { WeaponTypes.ShipCamouflage, new("Ship Camouflage", "Octopus Camouflage", ActionTypes.Defense, 1_100_000, 125_000)},
         { WeaponTypes.OceanFort, new("Ocean Fort", "Great Barrier Reef", ActionTypes.Defense, 2_100_000, 250_000)},
+
+        //Sapper Tools
+        { WeaponTypes.DivingWeight, new("Diving Weight", "Barnacles", ActionTypes.Sapping, 40_000, 4000)},
+        { WeaponTypes.SeaweedSuit, new("Seaweed Suit", "Wooden Shell Patterns", ActionTypes.Sapping, 140_000, 15_000)},
+        { WeaponTypes.DivingRope, new("Diving Rope", "Crab Chain", ActionTypes.Sapping, 260_000, 29_000)},
+        { WeaponTypes.DivingBell, new("Diving Bell", "Crab Catapult", ActionTypes.Sapping, 500_000, 60_000)},
+        { WeaponTypes.DivingSpear, new("Diving Spear", "Larger Pincers", ActionTypes.Sapping, 1_100_000, 125_000)},
+        { WeaponTypes.DivingSuit, new("Diving Suit", "Hermit Crabs", ActionTypes.Sapping, 2_100_000, 250_000)},
+
+        //Sentry Tools
+
+        {WeaponTypes.WhaleOilLamps, new("Whale Oil Lamps", "Bioluminescent Krill", ActionTypes.Sentry, 40_000, 4000)},
+        {WeaponTypes.AlarmHorn, new("Alarm Horn", "Sonar Blast", ActionTypes.Sentry, 140_000, 15_000)},
+        {WeaponTypes.Spyglass, new("Spyglass", "Clownfish Lookouts", ActionTypes.Sentry, 260_000, 29_000)},
+        {WeaponTypes.Lookout, new("Lookout", "Siren", ActionTypes.Sentry, 500_000, 60_000)},
+        {WeaponTypes.CrowsNest, new("Crow's Nest", "Jellyfish Bloom", ActionTypes.Sentry, 1_100_000, 125_000)},
+        {WeaponTypes.SeeThroughHullSection, new("See-Through Hull Section", "Blue Whale Patrol", ActionTypes.Sentry, 2_100_000, 250_000)},
+
     }.ToImmutableDictionary();
 
     /// <summary> Get an enumerable of Weapons of a specific ActionType. </summary>
@@ -65,5 +83,17 @@ public enum WeaponTypes
     ReinforcedHullArmour,
     BlackbeardsTunic,
     ShipCamouflage,
-    OceanFort
+    OceanFort,
+    DivingWeight,
+    SeaweedSuit,
+    DivingRope,
+    DivingBell,
+    DivingSpear,
+    DivingSuit,
+    WhaleOilLamps,
+    AlarmHorn,
+    Spyglass,
+    Lookout,
+    CrowsNest,
+    SeeThroughHullSection
 };
