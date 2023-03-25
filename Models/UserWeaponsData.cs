6943aa5b7b53b3b18218295f31f3fd1ee0631409
Scott Parkhill <scottparkhill@culturefoundrystudios.com>
2023-03-25 10:48:32 -0400

    Update UserWeaponsData to use a class instead of tuple

--- Models/UserWeaponsData.cs
+++ Models/UserWeaponsData.cs
@@ -2,5 +2,13 @@ namespace Chaos.Models;
 
 public class UserWeaponsData
 {
-    List<Weapon> UserWeapons { get; set; } = new();
+    public List<UserWeapon> UserWeapons { get; set; } = new();
+}
+
+public class UserWeapon
+{
+    public int Count { get; set; }
+    public WeaponTypes WeaponType { get; init; }
+
+    public UserWeapon(int count, WeaponTypes weaponType) => (Count, WeaponType) = (count, weaponType);
 }
