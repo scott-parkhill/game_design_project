9c921b69485f451c49172b7384141e880284fb2d
Scott Parkhill <scottparkhill@culturefoundrystudios.com>
2023-03-25 12:00:27 -0400

    Add RequiredNotNegativeAttribute.cs

---
+++ Business/RequiredNotNegativeAttribute.cs
@@ -0,0 +1,23 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace Chaos.Business;
+
+public class RequiredNotNegativeAttribute : ValidationAttribute
+{
+    public override bool IsValid(object? value)
+    {
+        if (value is string inputNumber)
+        {
+            var result = Int32.TryParse(inputNumber, out int number);
+
+            if (result is false)
+                return false;
+            else if (number >= 0)
+                return true;
+            else
+                return false;
+        }
+        else
+            return false;
+    }
+}
