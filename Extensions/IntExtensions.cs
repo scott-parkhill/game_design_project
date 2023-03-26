bdefb7ca310413500ab64f0775e1119564fae944
Scott Parkhill <scottparkhill@culturefoundrystudios.com>
2023-03-25 20:37:20 -0400

    Add int extension for pretty printing numbers

---
+++ Extensions/IntExtensions.cs
@@ -0,0 +1,31 @@
+using System.Text;
+
+namespace Chaos.Extensions;
+
+public static class IntExtensions
+{
+    public static string PrettyPrintNumber(this int number)
+    {
+        StringBuilder sb = new();
+        string numberAsString = number.ToString();
+        numberAsString = new(numberAsString.Reverse().ToArray());
+
+        sb.Append(' ');
+
+        for (int i = 0; i < numberAsString.Length;)
+        {
+            sb.Append(numberAsString[i++]);
+
+            if (i < numberAsString.Length)
+                sb.Append(numberAsString[i++]);
+
+            if (i < numberAsString.Length)
+                sb.Append(numberAsString[i]);
+
+            if (i++ < numberAsString.Length - 1)
+                sb.Append(' ');
+        }
+
+        return new(sb.ToString().Reverse().ToArray());
+    }
+}
