31a035ce525994aee8a5f2a180d233f8a1e75059
Scott Parkhill <scottparkhill@culturefoundrystudios.com>
2023-03-26 13:02:29 -0400

    Update army score to use army not weapons

--- Business/ArmyScore.cs
+++ Business/ArmyScore.cs
@@ -1,71 +1,78 @@
 using Chaos.Models;
+using Chaos.Models.ViewModels;
 
 namespace Chaos.Business;
 
 public static class ArmyScore{
+    const double RecruitBaseRate = 1;
+    const double TrainedBaseRate = 2;
 
     /// <summary> Get single value representing overall army score. </summary>
-    public static double GetArmyScore(UserWeaponsData userWeapons, double[]? modifiers = null) => AllScores(userWeapons, modifiers).Sum();
+    public static double GetArmyScore(ArmyViewModel army, double[]? modifiers = null) => GetArmyScores(army, modifiers).Sum();
 
     /// <summary> Format for the modifiers array: [attack, defence, sentry, sapper] (same as ActionTypes). </summary>
-    public static double[] AllScores(UserWeaponsData userWeapons, double[]? modifiers = null){
+    public static double[] GetArmyScores(ArmyViewModel army, double[]? modifiers = null){
         modifiers ??= new double[] { 1,1,1,1 };
         return new double[]{
-            AttackScore(userWeapons, modifiers[(int)ActionTypes.Offense]),
-            DefenceScore(userWeapons, modifiers[(int)ActionTypes.Defence]),
-            SentryScore(userWeapons, modifiers[(int)ActionTypes.Sentry]),
-            SapperScore(userWeapons, modifiers[(int)ActionTypes.Sapping])
+            AttackScore(army, modifiers[(int)ActionTypes.Offence]),
+            DefenceScore(army, modifiers[(int)ActionTypes.Defence]),
+            SentryScore(army, modifiers[(int)ActionTypes.Sentry]),
+            SapperScore(army, modifiers[(int)ActionTypes.Sapping])
         };
     }
 
     //formula is (weapon count) * (weapon strenghth) * (normalization value);
 
     //modifier is the normalization value that will be used to apply the buffs or debuffs
-    private static double AttackScore(UserWeaponsData weaponsData, double modifier){
+    private static double AttackScore(ArmyViewModel army, double modifier){
         double score = 0;
 
-        var values = weaponsData.GetWeaponsByActionType(Business.ActionTypes.Offense);
+        var unitModifier = army.Attackers * TrainedBaseRate + army.Recruits * RecruitBaseRate;
+        var values = army.UserWeapons.GetWeaponsByActionType(Business.ActionTypes.Offence);
 
         foreach(var item in values){
             score += item.Count * item.Weapon.Strength;
         }
 
-        return score * modifier;
+        return (score + unitModifier) * modifier;
     }
 
-    private static double DefenceScore(UserWeaponsData weaponsData, double modifier){
+    private static double DefenceScore(ArmyViewModel army, double modifier){
         double score = 0;
 
-        var values = weaponsData.GetWeaponsByActionType(Business.ActionTypes.Defence);
+        var unitModifier = army.Defenders * TrainedBaseRate + army.Recruits * RecruitBaseRate;
+        var values = army.UserWeapons.GetWeaponsByActionType(Business.ActionTypes.Defence);
 
         foreach(var item in values){
             score += item.Count * item.Weapon.Strength;
         }
 
-        return score * modifier;
+        return (score + unitModifier) * modifier;
     }
 
-    private static double SentryScore(UserWeaponsData weaponsData, double modifier){
+    private static double SentryScore(ArmyViewModel army, double modifier){
         double score = 0;
 
-        var values = weaponsData.GetWeaponsByActionType(Business.ActionTypes.Sentry);
+        var unitModifier = army.Sentries * TrainedBaseRate + army.Recruits * RecruitBaseRate;
+        var values = army.UserWeapons.GetWeaponsByActionType(Business.ActionTypes.Sentry);
 
         foreach(var item in values){
             score += item.Count * item.Weapon.Strength;
         }
 
-        return score * modifier;
+        return (score + modifier) * modifier;
     }
 
-    private static double SapperScore(UserWeaponsData weaponsData, double modifier){
+    private static double SapperScore(ArmyViewModel army, double modifier){
         double score = 0;
 
-        var values = weaponsData.GetWeaponsByActionType(Business.ActionTypes.Sapping);
+        var unitModifier = army.Sappers * TrainedBaseRate + army.Recruits * RecruitBaseRate;
+        var values = army.UserWeapons.GetWeaponsByActionType(Business.ActionTypes.Sapping);
 
         foreach(var item in values){
             score += item.Count * item.Weapon.Strength;
         }
 
-        return score * modifier;
+        return (score + modifier) * modifier;
     }
 }
